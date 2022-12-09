b99e80a573e32635a470ab4a33995e8cfc2a388c
Ali Qornan <aliqornan97@gmail.com>
2022-12-10 05:17:01 +0700

    [Ali] Directly define return type by returning the kotlin object

--- server-kotlin/src/main/kotlin/aliqornan/grpc/example/server/CountryService.kt
+++ server-kotlin/src/main/kotlin/aliqornan/grpc/example/server/CountryService.kt
@@ -1,15 +1,18 @@
 package aliqornan.grpc.example.server
 
-import aliqornan.grpc.example.protocontract.*
+import aliqornan.grpc.example.protocontract.CountryServiceGrpcKt
+import aliqornan.grpc.example.protocontract.LookupRequest
+import aliqornan.grpc.example.protocontract.country
+import aliqornan.grpc.example.protocontract.lookupReply
 
 class CountryService : CountryServiceGrpcKt.CountryServiceCoroutineImplBase() {
 
-    override suspend fun lookup(request: LookupRequest): LookupReply = lookupReply {
+    override suspend fun lookup(request: LookupRequest) = lookupReply {
         data = findCountryByCode(request.code)
         success = true
     }
 
-    private fun findCountryByCode(countryCode: String): Country = country {
+    private fun findCountryByCode(countryCode: String) = country {
         code = "ID"
         officialName = "Republik Indonesia"
         numberOfPopulation = 2000000
